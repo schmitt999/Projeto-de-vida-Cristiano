0e74ce6f591a962058e20ffbe21568418f461118
schmitt999 <schmitt.cristiano@escola.pr.gov.br>
2026-04-23 11:16:39 -0300

    salvo

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ textos[i].classList.add("ativo");
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo2 = new Date("2025-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo4 = new Date("2020-10-05T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
@@ -47,6 +47,7 @@ document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
 document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
 document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
 document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
+console.log("seg"+i);
 }
 }
 
